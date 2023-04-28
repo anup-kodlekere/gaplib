c279d584cf368ac787b17c89f35d8fd7a4770042
Anup Kodlekere <100858233+anup-kodlekere@users.noreply.github.com>
2023-04-28 21:25:51 +0530

    [skip ci] Create setup-lxd-container.sh

---
+++ setup-lxd-container.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+ensure_lxd() {
+  if ! command -v lxc version &> /dev/null
+  then
+    echo "LXD could not be found. Please ensure LXD exists."
+    exit
+  fi
+}
+
+build_image_in_container() {
+  lxd launch ubuntu:22.04 gha-builder
+  lxc ls
+  
+  echo "Copy the build-image script into gha-builder"
+  lxc file push build-image.sh gha-builder/home/ubuntu/
+  
+  echo "Setting executable permissions on build-image.sh"
+  lxc exec gha-builder -- chmod +x /home/ubuntu/build-image.sh
+  
+  echo "Running build-image.sh"
+  lxc exec gha-builder -- /home/ubuntu/build-image.sh
+  
+  echo "Image build complete, deleting container"
+  lxc delete -f gha-builder
+
+}
+
+run() {
+  ensure_lxd
+  build_image_in_container
+}
+
+run "$@"
