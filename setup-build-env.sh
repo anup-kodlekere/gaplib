a31a97e348a56622e939bc49ffd267ee267f1f49
Anup Kodlekere <100858233+anup-kodlekere@users.noreply.github.com>
2023-06-23 16:01:14 +0530

    [skip ci] Update setup-build-env.sh

--- setup-build-env.sh
+++ setup-build-env.sh
@@ -14,29 +14,30 @@ build_image_in_container() {
   local DOTNET_SDK="dotnet-sdk-7.0.100-linux-ppc64le.tar.gz"
   local PATCH_FILE="runner-ppc64le-final.patch"
 
-  lxc launch ubuntu:20.04 gha-builder-$(date +%s)
+  local BUILD_CONTAINER=gha-builder-$(date +%s)
+  lxc launch ubuntu:20.04 "${BUILD_CONTAINER}"
   lxc ls
   
   # give container some time to wake up
   sleep 10
   
   echo "Copy the build-image script into gha-builder"
-  lxc file push "${BUILD_PREREQS_PATH}/build-image.sh" gha-builder/home/ubuntu/
+  lxc file push "${BUILD_PREREQS_PATH}/build-image.sh"  "${BUILD_CONTAINER}/home/ubuntu/"
   
   echo "Copy the dotnet-sdk into gha-builder"
-  lxc file push "${BUILD_PREREQS_PATH}/${DOTNET_SDK}" gha-builder/home/ubuntu/
+  lxc file push "${BUILD_PREREQS_PATH}/${DOTNET_SDK}" "${BUILD_CONTAINER}/home/ubuntu/"
   
   echo "Copy the patch file into gha-builder"
-  lxc file push "${BUILD_PREREQS_PATH}/${PATCH_FILE}" gha-builder/home/ubuntu/
+  lxc file push "${BUILD_PREREQS_PATH}/${PATCH_FILE}" "${BUILD_CONTAINER}/home/ubuntu/"
   
   echo "Setting executable permissions on build-image.sh"
-  lxc exec gha-builder -- chmod +x /home/ubuntu/build-image.sh
+  lxc exec "${BUILD_CONTAINER}" -- chmod +x /home/ubuntu/build-image.sh
   
   echo "Running build-image.sh"
-  lxc exec gha-builder -- /home/ubuntu/build-image.sh
+  lxc exec "${BUILD_CONTAINER}" -- /home/ubuntu/build-image.sh
   
-  echo "Image build complete, deleting container"
-  lxc delete -f gha-builder
+  # echo "Image build complete, deleting container"
+  # lxc delete -f gha-builder
 
 }
 
