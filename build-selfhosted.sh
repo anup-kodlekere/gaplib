ff832887c51223cf0afd6771edf70a647110dee1
Neale Ferguson <neale@sinenomine.net>
2024-01-29 22:14:28 -0500

    Add self-hosted actions runner build and instructions
    * Dockerfile.almalinux
      Dockerfile.ubuntu
      Dockerfile.opensuse
      - Self-hosted OCI build
    
    * README.md
      - Add build, configuration, and run instructions
    
    * build-files/convert-rpm.sh
      - Used in build of runner:ubuntu to get and convert dotnet RPMs
    
    * build-selfhosted.sh
      - Build OCI for ubuntu and almalinux-based self-hosted actions runners
    
    * selfhostedrunner.pngo
      - Documentation
    
    * setup-build-env.sh
      - Minor update
    
    * Dockerfile.test
      - Sample file to build a running actions runner
    
    * CI_Job.png
      - Sample screenshot of CI job completion

---
+++ build-selfhosted.sh
@@ -0,0 +1,47 @@
+#!/bin/bash
+usage() {
+    echo "Usage: build-standalone.sh [flags] [ubuntu|almalinux]"
+    echo
+    echo "Where flags:"
+    echo "-b [docker|podman]           Image build tool to use - defaults to which it finds first"
+    echo "-h                           Display this usage information"
+    echo
+    echo "If no distribution is specified then images for both are built"
+    exit
+}
+
+ARCH=`uname -m`
+DISTROS=""
+BUILDER=`which podman 2>/dev/null`
+if [ -z ${BUILDER} ]; then
+    BUILDER=`which docker 2>/dev/null`
+fi
+if [ -z ${BUILDER} ]; then
+    echo "Need podman or docker installed" >&2
+    exit 1
+fi
+while getopts "b:h" opt
+do
+    case "${opt}" in
+        b)
+            BUILDER="${OPTARG}"
+            ;;
+        h)
+            usage
+            ;;
+    esac
+done
+shift $(( OPTIND - 1 ))
+if [ -z "$@" ]; then
+    DISTROS="ubuntu almalinux opensuse"
+else
+    DISTROS=$@
+fi
+for dist in ${DISTROS}
+do
+    if [ ! -f Dockerfile.${dist} ]; then
+        echo "${dist} not supported" >&2
+    else 
+        ${BUILDER} build -f Dockerfile.${dist} --build-arg RUNNERPATCH=build-files/runner-${ARCH}.patch --tag runner:${dist} .
+    fi
+done
