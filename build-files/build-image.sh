1b36f5042165f03b9636f57d444aa9864787f2b7
rahulssv-ibm <rahul.vishwakarma2@ibm.com>
2024-07-03 14:00:06 +0530

    update

--- build-files/build-image.sh
+++ build-files/build-image.sh
@@ -3,21 +3,6 @@
 update_fresh_container() {
     echo "Upgrading and installing packages"
     sudo DEBIAN_FRONTEND=noninteractive apt-get -qq update -y
-
-    # Add Docker's official GPG key:
-    sudo apt-get install ca-certificates curl -y
-    sudo install -m 0755 -d /etc/apt/keyrings -y
-    sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
-    sudo chmod a+r /etc/apt/keyrings/docker.asc
-
-    # Add the repository to Apt sources:
-    echo \
-    "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/ubuntu \
-    $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | \
-    sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
-    sudo apt-get update -y
-
-    sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin -y
     sudo DEBIAN_FRONTEND=noninteractive apt-get -qq install alien libicu70 -y
     
     if [ $? -ne 0 ]; then
@@ -33,7 +18,6 @@ update_fresh_container() {
 
     echo "Make sure we have lxd authority"
     sudo usermod -G lxd -a ubuntu
-    sudo chmod 666 /var/run/docker.sock
 }
 
 setup_dotnet_sdk() {
