8039988c86bf1fe6a555a6d0129e4e46e1201704
Ilya Leoshkevich <iii@linux.ibm.com>
2024-07-31 00:58:47 +0200

    Fix the Ubuntu build on s390x
    * convert-rpm.sh uses hardcoded package versions, which can no longer
      be found each time AlmaLinux is updated. Always fetch the current
      versions.
    
    * Fix ShellCheck warnings in this script.
    
    * Don't silence wget output so that the script is debuggable.
    
    * Remove the unnecessary "exit 0" at the end.
    
    * Account for alien errors like the following:
    
        Unpacking of 'dotnet-host-8.0.4-2.el9_4.s390x.rpm' failed at /usr/share/perl5/Alien/Package/Rpm.pm line 168.
    
      and add a workaround from [1].
    
    * Rebase runner-s390x.patch.
    
    * Use SDK 8 on s390x, since that's what the latest runner requires.
    
    * Move ARGS a bit lower in the Dockerfile to improve caching.
    
    * Turn off package signature verification. AlmaLinux's .NET may contain
      an outdated trust store. See [2] for details.
    
    * Fix the packs symlinks.
    
    * Pass /p:SelfContained=false to the test runner in order to avoid
      apphost issues in ./dev.sh test.
    
    [1] https://github.com/anup-kodlekere/gaplib/issues/14
    [2] https://learn.microsoft.com/en-us/dotnet/core/tools/nuget-signed-package-verification

--- build-files/convert-rpm.sh
+++ build-files/convert-rpm.sh
@@ -1,41 +1,42 @@
 #!/bin/bash
 . /etc/os-release
 SDK=$1
-ARCH=`uname -m`
-MIRROR="https://mirror.lchs.network/pub/almalinux/9.3/AppStream/${ARCH}/os/Packages"
-case "${SDK}" in
-    7)
-        PKGS="dotnet-apphost-pack-7.0-7.0.15-1.el9_3 dotnet-host-8.0.1-1.el9_3"
-        PKGS="${PKGS} dotnet-hostfxr-7.0-7.0.15-1.el9_3 dotnet-targeting-pack-7.0-7.0.15-1.el9_3"
-        PKGS="${PKGS} dotnet-templates-7.0-7.0.115-1.el9_3 dotnet-runtime-7.0-7.0.15-1.el9_3"
-        PKGS="${PKGS} dotnet-sdk-7.0-7.0.115-1.el9_3 aspnetcore-runtime-7.0-7.0.15-1.el9_3"
-        PKGS="${PKGS} aspnetcore-targeting-pack-7.0-7.0.15-1.el9_3 netstandard-targeting-pack-2.1-8.0.101-1.el9_3"
-        ;;
-    6)
-        PKGS="dotnet-host-8.0.1-1.el9_3 dotnet-apphost-pack-6.0-6.0.26-1.el9_3"
-        PKGS="${PKGS} dotnet-hostfxr-6.0-6.0.26-1.el9_3 dotnet-targeting-pack-6.0-6.0.26-1.el9_3"
-        PKGS="${PKGS} dotnet-templates-6.0-6.0.126-1.el9_3 dotnet-runtime-6.0-6.0.26-1.el9_3"
-        PKGS="${PKGS} dotnet-sdk-6.0-6.0.126-1.el9_3 aspnetcore-runtime-6.0-6.0.26-1.el9_3"
-        PKGS="${PKGS} aspnetcore-targeting-pack-6.0-6.0.26-1.el9_3 netstandard-targeting-pack-2.1-8.0.101-1.el9_3"
-        ;;
-    *)
-        echo "Unsupported SDK ${SDK}" >&2
+ARCH=$(uname -m)
+MIRROR=https://mirror.lchs.network/pub/almalinux/9/AppStream/${ARCH}/os/Packages
+PKGS_HTML=$(wget --output-document=- "$MIRROR")
+PKG_NAMES=(
+    "dotnet-apphost-pack-${SDK}"
+    "dotnet-host-8"
+    "dotnet-hostfxr-${SDK}"
+    "dotnet-targeting-pack-${SDK}"
+    "dotnet-templates-${SDK}"
+    "dotnet-runtime-${SDK}"
+    "dotnet-sdk-${SDK}"
+    "aspnetcore-runtime-${SDK}"
+    "aspnetcore-targeting-pack-${SDK}"
+    "netstandard-targeting-pack-2.1-8"
+)
+RPMS=()
+for PKG_NAME in "${PKG_NAMES[@]}"; do
+    if [[ "$PKGS_HTML" =~ \"("$PKG_NAME"[^\"]*.rpm)\" ]]; then
+        RPM=${BASH_REMATCH[1]}
+        echo "Found $RPM"
+        RPMS+=("$RPM")
+    else
+        echo "$PKG_NAME not found" >&2
         exit 1
-        ;;
-esac
-echo "Retrieving dotnet packages"
-pushd /tmp >/dev/null
-for pkg in ${PKGS}
-do
-    RPM="${pkg}.${ARCH}.rpm"
-    wget -q ${MIRROR}/${RPM}
-    if [ ${ID} == "ubuntu" ]; then
-        echo -n "Converting ${RPM}... "
-        alien -d ${RPM} |& grep -v ^warning
-        if [ $? -ne 0 ]; then
-            exit 2
-        fi
-        rm -f ${RPM}
     fi
 done
-exit 0
+echo "Retrieving dotnet packages"
+pushd /tmp >/dev/null || exit 1
+for RPM in "${RPMS[@]}"
+do
+    wget "${MIRROR}/${RPM}" || exit 1
+    if [ "${ID}" == "ubuntu" ]; then
+        echo -n "Converting ${RPM}... "
+        if ! alien -d "${RPM}" |& grep -v -e ^warning -e ^Unpacking; then
+            exit 2
+        fi
+        rm -f "${RPM}"
+    fi
+done
