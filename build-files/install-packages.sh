18e219946fa618946474155a02458677e67e0c53
rahulssv-ibm <rahul.vishwakarma2@ibm.com>
2024-10-13 15:57:29 +0530

    update

--- build-files/install-packages.sh
+++ build-files/install-packages.sh
@@ -22,8 +22,8 @@ sudo DEBIAN_FRONTEND=noninteractive apt-get -qq update -y
 for pkg in docker.io docker-doc docker-compose docker-compose-v2 podman-docker containerd runc; do sudo apt-get remove $pkg; done
 
 # Add Docker's official GPG key:
-sudo apt-get update
-sudo apt-get install ca-certificates curl
+sudo DEBIAN_FRONTEND=noninteractive apt-get update -y 
+sudo DEBIAN_FRONTEND=noninteractive apt-get install ca-certificates curl -y
 sudo install -m 0755 -d /etc/apt/keyrings
 sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
 sudo chmod a+r /etc/apt/keyrings/docker.asc
@@ -33,9 +33,9 @@ echo \
   "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/ubuntu \
   $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | \
   sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
-sudo apt-get update
+sudo DEBIAN_FRONTEND=noninteractive apt-get update -y
 
-sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+sudo DEBIAN_FRONTEND=noninteractive apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin -y
 
 # Read the file line by line and install each package
 while IFS= read -r package || [ -n "$package" ]; do
