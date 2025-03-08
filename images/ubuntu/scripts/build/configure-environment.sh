4c8810bf081fa4374c29dc2946d383d0c548ae9a
rahulssv-ibm <rahul.vishwakarma2@ibm.com>
2025-03-08 19:31:42 +0530

    update: chaged mtu to 1400

--- images/ubuntu/scripts/build/configure-environment.sh
+++ images/ubuntu/scripts/build/configure-environment.sh
@@ -38,6 +38,8 @@ echo 'net.ipv6.conf.all.disable_ipv6=1' | tee -a /etc/sysctl.conf
 echo 'net.ipv6.conf.default.disable_ipv6=1' | tee -a /etc/sysctl.conf
 echo 'net.ipv6.conf.lo.disable_ipv6=1' | tee -a /etc/sysctl.conf
 
+ip link set dev $(ip route | awk '/default/ {print $5}') mtu 1400
+
 # https://www.elastic.co/guide/en/elasticsearch/reference/current/vm-max-map-count.html
 # https://www.suse.com/support/kb/doc/?id=000016692
 echo 'vm.max_map_count=262144' | tee -a /etc/sysctl.conf
