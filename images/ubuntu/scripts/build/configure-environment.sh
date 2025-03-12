30191e6f2e0a002beb117ecff1ff3ce24673f1b0
rahulssv-ibm <rahul.vishwakarma2@ibm.com>
2025-03-12 11:04:00 +0530

    update

--- images/ubuntu/scripts/build/configure-environment.sh
+++ images/ubuntu/scripts/build/configure-environment.sh
@@ -38,7 +38,6 @@ echo 'net.ipv6.conf.all.disable_ipv6=1' | tee -a /etc/sysctl.conf
 echo 'net.ipv6.conf.default.disable_ipv6=1' | tee -a /etc/sysctl.conf
 echo 'net.ipv6.conf.lo.disable_ipv6=1' | tee -a /etc/sysctl.conf
 
-ip route
 # https://www.elastic.co/guide/en/elasticsearch/reference/current/vm-max-map-count.html
 # https://www.suse.com/support/kb/doc/?id=000016692
 echo 'vm.max_map_count=262144' | tee -a /etc/sysctl.conf
